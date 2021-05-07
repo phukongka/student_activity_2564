fc9836c0d9079d44cb1a66fb67eeb077286281f6
phukongka <aphukong@gmail.com>
2021-05-07 16:26:24 +0700

    init1

---
+++ includes/leftbar.php
@@ -0,0 +1,56 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">              
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li><a href="manage-users.php"><i class="fa fa fa-users"></i> <span> User</span></a></li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>รายงานการส่งข้อมูล</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="manage-homeroom.php"><i class="fa fa-bars"></i> <span>กิจกรรมโฮมรูม</span></a></li>
+                                            <li><a href="#"><i class="fa fa fa-server"></i> <span>กิจกรรมหน้าเสาธง</span></a></li>
+                                            <li><a href="#"><i class="fa fa fa-server"></i> <span>กิจกรรมเยี่ยมบ้านผู้เรียน</span></a></li>
+                                        </ul>
+                                    </li>
+  <!-- <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Subjects</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li> -->
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Timeline</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-time.php"><i class="fa fa-bars"></i> <span>Add Time</span></a></li>
+                                            <li><a href="manage-time.php"><i class="fa fa fa-server"></i> <span>Manage Time</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-profile.php"><i class="fa fa fa-users"></i> <span> Admin Change Profile</span></a></li>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Admin Change Password</span></a></li>  
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap4/css/bootstrap.min.css">
+	<script type="text/javascript" src="jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark">
+		<div class="container">
+			<a href="#" class="navbar-brand" style="font-size: 28px;"><strong>ระบบกิจกรรมนักเรียน นักศึกษา</strong></a>
+			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#collapse1">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="collapse1">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<button class="btn btn-sm btn-warning" data-target="#mylogin" data-toggle="modal">เข้าสู่ระบบ</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6" style="margin-top: 10px;">
+				<center><p style="font-size: 55px;"><strong>ระบบกิจกรรม</strong>
+						<strong><p style="font-size: 58px; margin-top: -20px;">นักเรียน นักศึกษา</p></strong>
+						<strong><p style="font-size: 22px; margin-top: -20px;">Chaiyaphum Technical Colleges</p></strong>
+						<img src="images/P1000504.JPG" class="rounded img-fluid">
+				</center>
+			</div>
+			<div class="col-md-6" style="margin-top: 90px;">
+				<div class="card">
+					<div class="card-header">
+						<strong>
+							<p style="font-size: 40px;">สร้างบัญชีใหม่</p>
+							<p style="font-size: 25px; margin-top: -20px;">ง่ายและเร็ว</p>
+						</strong>
+					</div>
+					<div class="card-body">
+						<form method="post">
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text"><i class="fas fa-user"></i></span>
+								</div>
+										<input class="form-control" type="text" name="user_name" placeholder="ชื่อ" required>
+										<input class="form-control" type="text" name="user_lastname" placeholder="นามสกุล" required>
+							</div><br>
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text"><i class="fas fa-key"></i></span>
+								</div>
+										<input class="form-control" pattern=".{6,}" type="password" name="user_pass" placeholder="รหัสผ่าน"required>
+							</div><br>
+							<div class="input-group border-5">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="fas fa-envelope-open-text"></i></span>
+									</div>
+										<input class="form-control" type="text" name="user_email" placeholder="Email" required>
+								</div><br>
+								<div class="input-group border-5">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="fas fa-globe"></i></span>
+									</div>
+										<select class="form-control" name="user_country">
+											<option>Pakistan</option>
+											<option>Thailand</option>
+										</select>
+								</div><br>
+								<div class="input-group border-5">
+									<div class="input-group-prepend">
+										<span class="input-group-text"><i class="fas fa-venus-mars"></i></span>
+									</div>
+										<select class="form-control" name="user_gender">
+											<option>ชาย</option>
+											<option>หญิง</option>
+											<option>ไม่ระบุ</option>
+										</select>
+								</div><br>
+								<button type="submit" name="reg" class="btn btn-info form-control">สมัคร</button>
+								<?php include("user-signup.php"); ?>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+		<!-- modal login -->
+		<div class="modal fade" id="mylogin">
+			<div class="modal-dialog modal-sm modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title">เข้าสู่ระบบ</h4>
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+						<div class="modal-body">
+							<form method="post">
+								<div class="input-group">
+									<div class="input-group-prepend">
+								      <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+								    </div>
+									<input class="form-control" type="text" name="email" placeholder="Enter Email">
+								</div><br>
+								<div class="input-group">
+									<div class="input-group-prepend">
+								      <span class="input-group-text"><i class="fas fa-key"></i></span>
+								    </div>
+									<input class="form-control" type="password" name="password" placeholder="Enter Password">
+								</div>
+						</div>
+					<div class="modal-footer">
+						<button type="submit" name="login" class="btn btn-block btn-info">Login</button>
+						<!-- <button class="btn btn-danger" data-dismiss="modal">Close</button> -->
+					</form>
+					</div>
+					<?php include("login.php"); ?>
+				</div>
+			</div>
+		</div>
+		
+</body>
+</html>
