dc58f8e519fc880ba7feff70c8f31ecc7bff1389
Pantakit-Joseph <mr.pantakit@gmail.com>
2021-05-20 10:03:39 +0700

    ui

--- includes/topbar.php
+++ includes/topbar.php
@@ -1,3 +1,14 @@
+<?php 
+// session_start();
+include "includes/config.inc.php";
+$user_id = $_SESSION['user_id'];
+$sql_bar = "SELECT * FROM general_user WHERE user_id = '$user_id'";
+$qu_bar = $conn->query($sql_bar);
+$row_bar = $qu_bar->fetch_array();
+$title =$row_bar['title'];
+$user_name =$row_bar['user_name'];
+$user_lastname = $row_bar['user_lastname'];
+?>
   <nav class="navbar top-navbar bg-white box-shadow">
             	<div class="container-fluid">
                     <div class="row">
@@ -18,20 +29,48 @@
 
                 		<div class="collapse navbar-collapse" id="navbar-collapse-1">
                 			<ul class="nav navbar-nav" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
-                                <li class="hidden-sm hidden-xs"><a href="#" class="user-info-handle"><i class="fa fa-user"></i></a></li>
+                                <li class="dropdown">
+									<a href="#" class="user-info-handle dropdown-toggle" id="AdminChange" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i></a>
+									<div class="dropdown-menu" aria-labelledby="AdminChange">
+										<a href="change_profile_admin.php" class="dropdown-item btn"><i class="fa fa fa-users"></i> <span> Admin Change Profile</span></a>
+										<a href="change-password-admin.php" class="dropdown-item btn"><i class="fa fa fa-server"></i> <span> Admin Change Password</span></a>
+									</div>
+								</li>
+
                                 <li class="hidden-sm hidden-xs"><a href="#" class="full-screen-handle"><i class="fa fa-arrows-alt"></i></a></li>
                        
-                				<li class="hidden-xs hidden-xs"><!-- <a href="#">My Tasks</a> --></li>
+                				<li class="hidden-xs hidden-xs"><a href="#"><?php echo $title." ".$user_name." ".$user_lastname;?></a></li>
                                
                 			</ul>
                             <!-- /.nav navbar-nav -->
 
                 			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
-                             
-                				
+                
+								<!-- <li class="hidden-sm hidden-xs dropdown">
+									<a href="#" class="user-info-handle dropdown-toggle" id="user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-plus"></i></a>
+									<div class="dropdown-menu" aria-labelledby="user">
+									<?php
+										$sql_request = "SELECT * FROM general_user WHERE user_status = 'request'";
+										$qu_request = $conn->query($sql_request);
+										while ($row_request = $qu_request->fetch_array()) {
+											$title =$row_request['title'];
+											$user_name =$row_request['user_name'];
+											$user_lastname = $row_request['user_lastname'];
+									?>
+											<div class="dropdown-item" style="padding: 10px; width: 350px;">
+												<?php echo "$title"." ".$user_name." ".$user_lastname; ?>
+												<button class="pull-right btn">อนุมัติ</button>
+											</div>
+									<?php
+										}
+									?>
+										
+									</div>
+								</li> -->
+
                 				    <li><a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Logout</a></li>
                 					
-                		
+
                             
                 			</ul>
                             <!-- /.nav navbar-nav navbar-right -->
